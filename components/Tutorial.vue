53c1931b0143342c964d7e75aded7647216f67ac
Abel Tran <abeltran.develop@gmail.com>
2021-07-30 22:36:20 +0700

    Initial

---
+++ components/Tutorial.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+   <div class="navbar">
+     Hello World
+   </div>
+  </div>
+</template>
+
+<style lang="scss">
+.navbar {
+  color: rebeccapurple;
+  @apply text-9xl;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Menubar />
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+<script>
+import Footer from './Footer';
+import Menubar from './Menubar';
+export default {
+  components: { Menubar, Footer }
+};
+</script>
